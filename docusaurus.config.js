4417755a87bc365cb0683c8e224d1302505af77f
slsnow <66100515+slsnow@users.noreply.github.com>
2022-11-20 03:15:21 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
 
-  url: 'https://slsnow.github.io', // Your website URL
+  url: 'https://github.com', // Your website URL
   baseUrl: '/snowball/',
   trailingSlash: false,
 
